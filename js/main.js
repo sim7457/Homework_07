223a6ba281fb5237211580c74139f84a5178884b
sim7457 <118698648+sim7457@users.noreply.github.com>
2022-12-30 17:02:37 +0900

    20221230 17:02

--- js/main.js
+++ js/main.js
@@ -9,11 +9,29 @@ $(function () {
         }
     });
 
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() > 150) {
-            $('.content-feature').addClass('on')
+    // $(window).on('scroll', function () {
+    //     if ($(window).scrollTop() > 150) {
+    //         $('.content-feature').addClass('on')
+    //     } else {
+    //         $('.content-feature').removeClass('on')
+    //     }
+    // });
+
+
+    $(window).on('scroll wheel', function (e) {
+        console.log(e, e.originalEvent.deltaY);
+        if ($(window).scrollTop() > 120) {
+            $('.content-feature').addClass('on');
+            if (e.originalEvent.deltaY > 0) {
+                $('#header').css({ top: -72 });
+                $('.content-feature').css({ top: 0 });
+            }
         } else {
-            $('.content-feature').removeClass('on')
+            $('.content-feature').removeClass('on');
+            if (e.originalEvent.deltaY < 0) {
+                $('#header').css({ top: 0 });
+                $('.content-feature').css({ top: 72 });
+            }
         }
     });
 
